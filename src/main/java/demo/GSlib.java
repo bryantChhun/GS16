f12eafb0d0f042a9b9232244ef93dd0cc0736024
Bryant <Bryant@github.com>
2018-06-14 14:32:08 -0700

    First Commit

---
+++ src/main/java/demo/GSlib.java
@@ -0,0 +1,15 @@
+package demo;
+
+import GS16AO64C.bindings.GS16AO64CLibrary;
+
+
+public class GSlib
+{
+
+    private static GS16AO64CLibrary INSTANCE = GS16AO64CLibrary.INSTANCE;
+
+    public GSlib()
+    {
+        System.out.print("nothing");
+    }
+}
